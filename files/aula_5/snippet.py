f6c9c8c721c135bb9d0b2b802c65c05ad48a6f26
root <root@DESKTOP-EIR2U7C.localdomain>
2021-01-10 16:38:49 -0300

    updated material

---
+++ files/aula_5/snippet.py
@@ -0,0 +1,18 @@
+genetic_code = { 
+  'ata':'i', 'atc':'i', 'att':'i', 'atg':'m', 
+  'aca':'t', 'acc':'t', 'acg':'t', 'act':'t', 
+  'aac':'n', 'aat':'n', 'aaa':'k', 'aag':'k', 
+  'agc':'s', 'agt':'s', 'aga':'r', 'agg':'r',
+  'cta':'l', 'ctc':'l', 'ctg':'l', 'ctt':'l', 
+  'cca':'p', 'ccc':'p', 'ccg':'p', 'cct':'p', 
+  'cac':'h', 'cat':'h', 'caa':'q', 'cag':'q', 
+  'cga':'r', 'cgc':'r', 'cgg':'r', 'cgt':'r', 
+  'gta':'v', 'gtc':'v', 'gtg':'v', 'gtt':'v', 
+  'gca':'a', 'gcc':'a', 'gcg':'a', 'gct':'a', 
+  'gac':'d', 'gat':'d', 'gaa':'e', 'gag':'e', 
+  'gga':'g', 'ggc':'g', 'ggg':'g', 'ggt':'g', 
+  'tca':'s', 'tcc':'s', 'tcg':'s', 'tct':'s', 
+  'ttc':'f', 'ttt':'f', 'tta':'l', 'ttg':'l', 
+  'tac':'y', 'tat':'y', 'taa':'*', 'tag':'*', 
+  'tgc':'c', 'tgt':'c', 'tga':'*', 'tgg':'w', 
+}
